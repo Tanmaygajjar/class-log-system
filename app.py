70def9f4f62c0e2151b62d5ef45dea6d179eb392
Tanmay Gajjar <gajjartanmayh@gmail.com>
2026-03-06 01:06:18 +0530

    fixed sqlite import

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.secret_key = "secret"
